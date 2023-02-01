5cd179591fa4e4bd9fb769ed314f67dacfee6f79
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-01 16:00:12 +0100

    bottoni verdi fatti

--- assets/script/artistPage.js
+++ assets/script/artistPage.js
@@ -85,11 +85,27 @@ songGenerator(120);
 
 //Funzioni extra al click
 
-let pressedElem = document.querySelectorAll(".press");
-/* pressedElem.forEach((el) => {
-    el.classList.toggle("push"); */
-for (let i = 0; i < pressedElem.length; i++) {
-  function pressGreen() {
-    pressedElem.classList.toggle("push");
-  }
+let pressedElem1 = document.getElementById("press1");
+let pressedElem2 = document.getElementById("press2");
+let pressedElem3 = document.getElementById("press3");
+let pressedElem4 = document.getElementById("press4");
+
+function pressGreen1() {
+  pressedElem1.classList.toggle("push");
 }
+pressedElem1.addEventListener("click", pressGreen1);
+
+function pressGreen2() {
+  pressedElem2.classList.toggle("push");
+}
+pressedElem2.addEventListener("click", pressGreen2);
+
+function pressGreen3() {
+  pressedElem3.classList.toggle("push");
+}
+pressedElem3.addEventListener("click", pressGreen3);
+
+function pressGreen4() {
+  pressedElem4.classList.toggle("push");
+}
+pressedElem4.addEventListener("click", pressGreen4);
