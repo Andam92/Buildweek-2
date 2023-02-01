3e806aaeedd83c4ebf9df5bd6a42e598475409eb
Antonio D'Amico <andam@hotmail.it>
2023-02-01 09:51:32 +0100

    responsive and js fixed on album

--- assets/script/albumPage.js
+++ assets/script/albumPage.js
@@ -15,7 +15,7 @@ let apiLink = "https://striveschool-api.herokuapp.com/api/deezer/album";
 
 // PER GENERARE NOME ALBUM E IMMAGINE
 
-const albumPicker = function (title, image, length, tracks) {
+const albumPicker = function (title, image, length, tracks, profileImg) {
   typeof title === "string" || typeof title === "number"
     ? (albumNameReference.innerText = String(title))
     : (albumNameReference.innerHTML = `non hai inserito il nome di un album!`);
@@ -24,7 +24,7 @@ const albumPicker = function (title, image, length, tracks) {
   albumImgReference.src = `${image}`;
   albumAuthorandDateReference.innerText = `durata: ${length} ·`;
   albumTracksNumberReference.innerText = `${tracks} brani ·`;
-  /* authorImg.src = `${profileImg}`; */
+  authorImg.src = `${profileImg}`;
 };
 
 const albumAssign = function (albumName) {
@@ -40,25 +40,30 @@ const albumAssign = function (albumName) {
         element.title,
         element.cover_medium,
         (element.duration / 60).toFixed(2),
-        element.nb_tracks
+        element.nb_tracks,
+        element.artist.picture_small
       );
+
+      // FUNZIONE PER GENERARE LE CANZONI DELL'ALBUM
       let trackList = element.tracks.data;
       console.log(trackList);
       tracksContainerReference.innerHTML = ``;
       for (let index = 0; index < trackList.length; index++) {
         tracksContainerReference.innerHTML += `<div class="row">
               <div class="d-flex justify-content-between align-items-center">
-              <div class="d-flex align-items-center">
+              <div class="d-flex align-items-center  col-6">
                     <p class="opacity-75 d-none d-md-block">${index + 1}</p>
                     <div class="ms-0 ms-md-3">
-                      <p class="m-0">FETCH 2!!!</p> 
-                      <p id="authorName" class="opacity-75 mt-1">FETCH 2!!!</p>
+                      <p class="m-0">${trackList[index].title}</p> 
+                      <p id="authorName" class="opacity-75 mt-1">${
+                        trackList[index].artist.name
+                      }</p>
                     </div>
                   </div>
-              <div > <p class="opacity-75 d-none d-md-block">${
+              <div class="col-3 justify-content-start d-flex" > <p class="opacity-75 d-none d-md-block">${
                 trackList[index].rank
               }</p> </div>
-              <div > <p class="opacity-75 d-none d-md-block">${
+              <div class="col-3 d-flex justify-content-end"> <p class="opacity-75 d-none d-md-block">${
                 trackList[index].duration > 59
                   ? `${(trackList[index].duration / 60).toFixed(2)} min`
                   : `${trackList[index].duration} sec`
@@ -66,12 +71,10 @@ const albumAssign = function (albumName) {
               <i class="bi bi-three-dots-vertical d-inline d-md-none fs-3 mb-2 pb-2 opacity-75"></i>
             </div>   
               </div>`;
-        if (index >= 4) break;
+        /*  if (index >= 4) break; */
       }
     });
 };
-
-// FUNZIONE PER GENERARE LE CANZONI DELL'ALBUM
 
 albumAssign(75621062);
 
